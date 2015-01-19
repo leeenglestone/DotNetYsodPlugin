37796b0c803d834250530d27be68ffab580b95ea
Lee Englestone <lee.englestone@gmail.com>
2015-01-19 08:40:41 +0000

    Updated to use better JavaScript

--- ysod.js
+++ ysod.js
@@ -2,25 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-	var heading1s = document.getElementsByTagName('h1');
-   
-	if(heading1s.length !=0)
-	{    
-		if(heading1s[0].innerText.indexOf('Server Error in') > -1)
-		{	
-			var cSharpRegEx = /(.?:\\.*?\.cs)/g;
-			var vbNetRegEx = /(.?:\\.*?\.vb)/g;
-		
-			if(cSharpRegEx.test(document.body.innerHTML))
-			{
-				document.body.innerHTML = document.body.innerHTML.replace(cSharpRegEx, '<a style="color:green; font-weight:bold;" href="file:///$1" target="_blank">$1</a>');
-			}
-			
-			if(vbNetRegEx.test(document.body.innerHTML))
-			{
-				document.body.innerHTML = document.body.innerHTML.replace(vbNetRegEx, '<a style="color:green; font-weight:bold;" href="file:///$1" target="_blank">$1</a>');
-			}
-				
-		}
-	}
+	(function ErrorFinder() {
+    
+  var regex = /(.?:\\.*?\.(vb|cs))/g,
+      errorLinkText = '<a style="color:green; font-weight:bold;" href="file:///$1" target="_blank">$1</a>';
+      
+  var IsYellowScreenOfDeath = function() {
+    var heading1s = document.getElementsByTagName('h1');
+    return (heading1s.length === 0 || heading1s[0].innerText.indexOf('Server Error in') > -1);
+  };
+  
+  var HasCSharpOrVbError = function() {
+    return (regex.test(document.body.innerHTML));
+  };
+  
+  var ReplaceFileErrorWithFileLink = function(){
+    document.body.innerHTML = document.body.innerHTML.replace(regex, errorLinkText);
+  };
+
+  if (!IsYellowScreenOfDeath() || !HasCSharpOrVbError)
+    return;
+  
+  ReplaceFileErrorWithFileLink();
+  
+}());
 	
